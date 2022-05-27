0ee210a8925fb0e7ed49840d1c85e2a7f0f380d5
Hana Amelia <Luminovoez@outlook.com>
2022-05-27 15:53:09 +0800

    js书签

--- scrap notes/javascript/google_to_vocabulary.js
+++ scrap notes/javascript/google_to_vocabulary.js
@@ -1,9 +1,12 @@
 javascript:(
     function(){
         var t = location.href;
-        var re = new RegExp("q=(\w+)&", "g");
+        // var re = new RegExp("q=(\\w+)&", "g");
+        var re = /q=(\w+)&/g; // alternative
         var arr = re.exec(t);
         var w = arr[1];
         location.href = "https://www.vocabulary.com/dictionary/" + w;
     }
 )();
+
+// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Guide/Regular_Expressions
