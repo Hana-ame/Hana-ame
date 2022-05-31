d3d069c9c4d1b4b3f4f248a5539f247ad14e4d52
Hana Amelia <Luminovoez@outlook.com>
2022-05-31 20:32:43 +0800

    尝试模糊头像

--- scrap notes/javascript.md
+++ scrap notes/javascript.md
@@ -7,3 +7,23 @@
 (function(){console.log(1)}());
 ```
 没差啊……
+
+
+# Xpath
+```
+var t = document.evaluate('//div[@class="h-threads-item uk-clearfixe"]//*[@class="h-threads-content"]', document, null, XPathResult.ANY_TYPE, null );
+```
+
+var t = document.evaluate('//div[@class="h-threads-item uk-clearfixe"]//*[@class="h-threads-content"]', document, null, XPathResult.ANY_TYPE, null );
+try {
+  var thisNode = iterator.iterateNext();
+
+  while (thisNode) {
+    console.log( thisNode.textContent );
+    thisNode.
+    thisNode = iterator.iterateNext();
+  }
+}
+catch (e) {
+  console.log( 'Error: Document tree modified during iteration ' + e );
+}
--- script/clear-display-name.js
+++ script/clear-display-name.js
@@ -1,10 +1,31 @@
-let c = document.getElementsByClassName(`display-name__account`);
-let re = /^(?:\s|\n)*(@\w+)/g; 
-for (let i=0;i<c.length;i++){
-    let e = c.item(i);
-    /* console.log(e.textContent); */
-    re.lastIndex = 0;
-    let arr = re.exec(e.textContent);
-    /* console.log(arr); */
-    e.textContent = arr[1];
-}
+javascript:(
+    function(){
+        {
+            let c = document.getElementsByClassName(`display-name__account`);
+            let re = /^(?:\s|\n)*(@\w+)/g; 
+            for (let i=0;i<c.length;i++){
+                let e = c.item(i);
+                /* console.log(e.textContent); */
+                re.lastIndex = 0;
+                let arr = re.exec(e.textContent);
+                /* console.log(arr); */
+                e.textContent = arr[1];
+            }
+        }        
+        
+        {
+            let iter = document.evaluate('//img[@class="u-photo account__avatar"]', document, null, XPathResult.ANY_TYPE, null );
+            try {
+                var thisNode = iter.iterateNext();              
+                while (thisNode) {
+                  console.log( thisNode.textContent );
+                  thisNode.style.setProperty('filter','blur(20px)')
+                  thisNode = iterator.iterateNext();
+                }
+            }
+            catch (e) {
+                console.log( 'Error: Document tree modified during iteration ' + e );
+            }
+        }
+    }
+)();
