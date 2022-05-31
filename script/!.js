4bfc706eed75a7f01a5bf14f43be25d8f105baf4
Hana Amelia <Luminovoez@outlook.com>
2022-05-31 21:02:53 +0800

    精简了一下函数

--- script/!.js
+++ script/!.js
@@ -1,1 +1,1 @@
-javascript:(function(){FILE_NAME=`[name-here].js`;let s=document.createElement('script');s.src=`https://gh.moonchan.xyz/Hana-ame/hana-ame/main/script/${FILE_NAME}`;document.body.appendChild(s);})();
+javascript:(function(){FILE_NAME=`clear-display-name.js`;let s=document.createElement('script');s.src=`https://gh.moonchan.xyz/Hana-ame/hana-ame/main/script/${FILE_NAME}`;document.body.appendChild(s);})();
