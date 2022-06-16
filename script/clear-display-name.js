36cb5e63691c88689d82cd19a19f52407868c935
Hana Amelia <Luminovoez@outlook.com>
2022-06-16 08:47:06 +0800

    修改脚本

--- script/clear-display-name.js
+++ script/clear-display-name.js
@@ -19,5 +19,31 @@ javascript:(function(){
   blurByXpath('//*[@class="h-card"]')
   blurByXpath('//bdi')
   blurByXpath('//*[@class="status__avatar"]')
-  blurByXpath('//*[@class="account__header__tabs__name"]')  
+  blurByXpath('//*[@class="account__header__tabs__name"]//small')  
+  blurByXpath('//*[@class="public-account-header__tabs__name"]//small')  
+
+  navigator.clipboard.writeText(`function blurByXpath(xpath){
+    let iter = document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null );
+    console.log(iter)
+    try {
+      var thisNode = iter.iterateNext();    
+      while (thisNode) {
+      console.log( thisNode.textContent );
+      thisNode.style.setProperty('filter','blur(4px)')
+      thisNode = iter.iterateNext();
+      }
+    }
+    catch (e) {
+      console.log(e);
+    }
+  }  
+  blurByXpath('//img[contains(@class,"u-photo")]')
+  blurByXpath('//*[@class="display-name"]')
+  blurByXpath('//*[@class="h-card"]')
+  blurByXpath('//bdi')
+  blurByXpath('//*[@class="status__avatar"]')
+  blurByXpath('//*[@class="account__header__tabs__name"]//small')  
+  blurByXpath('//*[@class="public-account-header__tabs__name"]//small')  
+  `);
 }());
+
