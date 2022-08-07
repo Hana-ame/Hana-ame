745f679d08ec1be284ca497d370d99d400a66446
Hana Amelia <Luminovoez@outlook.com>
2022-08-07 13:07:20 +0800

    bing.cn

--- script/google-to-vocabulary.js
+++ script/google-to-vocabulary.js
@@ -2,7 +2,7 @@ javascript:(
     function(){
         var t = location.href;
         // var re = new RegExp("q=(\\w+)&", "g");
-        var re = /q=(\w+)&?/g; // alternative
+        var re = /(?:search\?)?q=(\w+)&?/g; // alternative
         var arr = re.exec(t);
         var w = arr[1];
         location.href = "https://www.vocabulary.com/dictionary/" + w;
