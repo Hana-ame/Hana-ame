0ddb05b94eb71917df7edc0e07215bf1d8209f26
lumino <luminovoez@gmail.com>
2023-03-21 09:26:31 +0800

    modified:   merge.sh

--- merge.sh
+++ merge.sh
@@ -1,5 +1,12 @@
-echo $1
+CUR_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+echo $CUR_BRANCH
+
+git add .;
+git stash;
 
 git checkout main;
-git merge $1 --no-ff;
-git checkout $1;
+git merge $CUR_BRANCH --no-ff;
+git checkout $CUR_BRANCH;
+
+git stash pop;
