44c10d78a34a43274cff5bfef85a23993ed4a7a5
lumino <luminovoez@gmail.com>
2024-10-11 22:55:09 +0800

    input

--- input.py
+++ input.py
@@ -1,8 +1,11 @@
+import sys
 import pyautogui
 import time
 
 time.sleep(1)
 
-with open("input.txt", 'r', encoding='utf8') as f:
+fn = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
+
+with open(fn, 'r', encoding='utf8') as f:
   s = f.read()
   pyautogui.write(s, interval=0.005)  # 每个字符之间有 0.1 秒的间隔
