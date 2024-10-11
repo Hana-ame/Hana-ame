7db7ef1bb97f5912b536718edc685ba741790ba9
lumin <luminovoez@gmail.com>
2024-10-12 03:21:55 +0800

    allowed hosts

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ia8a8^y3)*5m$ez(q_38_mixom=@8&dm-rtuheuv-xc^)&-$4a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
