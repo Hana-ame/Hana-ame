47ea8d903bb422418801e8f506977ed59ae6d7ad
lumin <luminovoez@gmail.com>
2024-11-15 15:34:53 +0800

    use react router

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
 // src/App.js
-import React, { useState } from 'react';
+import React from 'react';
+import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 
 const App = () => {
-    const [currentView, setCurrentView] = useState('home'); // 默认视图
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">首页</Link>
+          </li>
+          <li>
+            <Link to="/about">关于我们</Link>
+          </li>
+        </ul>
+      </nav>
 
-    const renderView = () => {
-        switch (currentView) {
-            case 'home':
-                return <Home />;
-            case 'about':
-                return <About />;
-            default:
-                return <Home />;
-        }
-    };
-
-    return (
-        <div>
-            <nav>
-                <button onClick={() => setCurrentView('home')}>首页</button>
-                <button onClick={() => setCurrentView('about')}>关于我们</button>
-            </nav>
-            {renderView()}
-        </div>
-    );
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
