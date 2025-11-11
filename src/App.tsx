e28ae54501c624e75f61c69fd64e8b39c0cbc121
lumin <luminovoez@gmail.com>
2025-11-11 21:52:59 +0800

    manifest

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,16 @@ import { Routes, Route, Link, HashRouter } from 'react-router-dom';
 // import HomePage from './pages/HomePage.tsx';
 // import AboutPage from './pages/AboutPage.tsx';
 // 1. 使用 lazy 动态导入页面组件
-const HomePage = lazy(() => import('./pages/HomePage'));
-const AboutPage = lazy(() => import('./pages/AboutPage'));
+const ProxyUrl = lazy(() => import('./pages/proxy_url/App.jsx'))
 
 function App() {
   return (
     <HashRouter>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">首页</Link>
-          </li>
-          <li>
-            <Link to="/about">关于</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <hr />
 
       {/* 2. 使用 Suspense 包裹 Routes，并提供 fallback UI */}
       <Suspense fallback={<div>正在加载页面，请稍候...</div>}>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
+          <Route path="/" element={<ProxyUrl />} />
         </Routes>
       </Suspense>
     </HashRouter>
---
+++ src/pages/NavBar.jsx
@@ -0,0 +1,12 @@
+export default NavBar = () => (
+    <nav>
+        <ul>
+            <li>
+                <Link to="/">首页</Link>
+            </li>
+            <li>
+                <Link to="/about">关于</Link>
+            </li>
+        </ul>
+    </nav>
+)
